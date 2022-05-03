b7f9874fa5163998e6945ca022fac1c7e16d2ae6
Tedu <jeenkuo@163.com>
2022-05-03 16:53:48 +0800

    Namechange commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 这是一个测试git的markdown文件
 ## AID1908
+## ksfasf
 **加粗**
 ![](.imgs/._01.jpg)
--- day02_CSS/hello.py
+++
@@ -1,6 +0,0 @@
-print("hello world")
-import numpy as np
-a = np.array([1,2,3,4])
-print(type(a))
-for i in a:
-    print(i)
